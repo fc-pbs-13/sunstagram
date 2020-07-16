b48ca9c0f19087bca227f745bfb3d313b0756e24
hsw0905 <hsw0905@gmail.com>
2020-07-16 15:43:52 +0900

    issue : unique_together_validator can't pass

--- repo/sunstagram/likes/serializers.py
+++ repo/sunstagram/likes/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.validators import UniqueTogetherValidator
 
 from likes.models import PostLike, CommentLike, ReplyLike
 from profiles.serializers import PostingProfileSerializer
+from replies.models import Reply
 
 
 class PostLikeSerializer(serializers.ModelSerializer):
@@ -30,3 +33,11 @@ class ReplyLikeSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'reply']
         read_only_fields = ('id', 'user', 'reply')
 
+        # 이거 넣으면 duplicate test에서 400 error를 리턴하지만,
+        # create test에서도 400 error를 리턴, serializer.instance가 왜 none인지..
+        # validators = [
+        #     UniqueTogetherValidator(
+        #         queryset=ReplyLike.objects.all(),
+        #         fields=['user', 'reply']
+        #     )
+        # ]
